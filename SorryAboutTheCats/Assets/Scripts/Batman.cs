002956b392ee044824800f841859a148ed1585c4
kevin.armstrong <kevak@hotmail.se>
2018-05-21 17:18:07 +0200

    no message

---
+++ SorryAboutTheCats/Assets/Scripts/Batman.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Batman : MonoBehaviour
+{
+
+    public GameObject BatmanPlayer;
+    public Sprite BatmanSpriteCar;
+    public Sprite BatmanSpriteDestroyed;
+    private SpriteRenderer spriteRenderer;
+
+    public List<AudioClip> CatSounds = new List<AudioClip>();
+    public List<AudioClip> DogSounds = new List<AudioClip>();
+    public List<AudioClip> BatmanSounds = new List<AudioClip>();
+    
+
+    void Start()
+    {
+        // SoundFolder should be in Assets/Resources 
+        CatSounds.Add((AudioClip)Resources.Load(" SoundFolder/youraudioClip1"));
+        CatSounds.Add((AudioClip)Resources.Load(" SoundFolder/youraudioClip2"));
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+            spriteRenderer.sprite = BatmanSpriteCar;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.otherRigidbody.CompareTag("Cat"))
+        {
+            transform.GetComponent<AudioSource>().clip = CatSounds[Random.Range(0, CatSounds.Count)];
+            transform.GetComponent<AudioSource>().Play();
+            
+        }
+        if (collision.otherRigidbody.CompareTag("Dog"))
+        {
+            transform.GetComponent<AudioSource>().clip = DogSounds[Random.Range(0, DogSounds.Count)];
+            transform.GetComponent<AudioSource>().Play();
+        }
+
+    }
+    void ChangeNOW()
+    {
+        if (spriteRenderer.sprite == BatmanSpriteCar) // if the spriteRenderer sprite = sprite1 then change to sprite2
+        {
+            
+            spriteRenderer.sprite = BatmanSpriteDestroyed;
+        }
+        else
+        {
+            spriteRenderer.sprite = BatmanSpriteCar; // otherwise change it back to sprite1
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Hello");
+            ChangeNOW();
+        }
+    }
+}
+
