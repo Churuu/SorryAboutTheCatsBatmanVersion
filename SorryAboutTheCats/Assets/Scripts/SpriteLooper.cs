dd490f2ff091dad588d390855e0186feaee7854d
Adam Brodin <worecraft@gmail.com>
2018-05-21 16:35:17 +0200

    no message

---
+++ SorryAboutTheCats/Assets/Scripts/SpriteLooper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLooper : MonoBehaviour
+{
+    #region Variables
+    private Camera mainCam;
+    public float basePosition = 5;
+    public float moveSpeed = -1;
+    #endregion
+
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    void Update()
+    {
+        Movement();
+        BoundryCheck();
+    }
+
+    void BoundryCheck()
+    {
+        if (transform.position.y < -8)
+        {
+            transform.position = new Vector2(0, basePosition);
+        }
+    }
+
+    void Movement()
+    {
+        transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+    }
+}
