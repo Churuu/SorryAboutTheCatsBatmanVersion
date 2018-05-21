582b4c10712eb905eab7493ff79f99a59735ec76
Adam Brodin <worecraft@gmail.com>
2018-05-21 15:03:36 +0200

    no message

--- SorryAboutTheCats/Assets/Scripts/PlayerMovement.cs
+++ SorryAboutTheCats/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variables
+    private Rigidbody2D rb2d;
+    private int movementSpeed = 5; // 5 standard value
+    #endregion
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        KeyboardInput();
+    }
+
+    void KeyboardInput()
+    {
+        rb2d.velocity = new Vector2((Input.GetAxis("Horizontal") * movementSpeed), 0);
+    }
 }
