b927db3f531a3fb5991ce792cb756161f8601d1f
LEARNET\hanna.lofgren1 <hanlof90@gmail.com>
2018-05-22 08:32:06 +0200

    no message

---
+++ SorryAboutTheCats/Assets/QuitScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitSprict : MonoBehaviour {
+
+
+    public void Quit()
+    {
+        {
+            Application.Quit();
+            print("Quit");
+        }
+    }
+
+
+}
+
